1c33ea4c45e8b24e3998baa7b449f240720acee7
Md. Masud Rana <mr.hasib45@gmail.com>
2022-01-12 22:52:07 +0600

    added a function

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import Contact from './Contact';
-
+import React from "react";
+import Contact from "./Contact";
 
 const Contacts = () => {
-    return (
-        <div>
-            <h1>Contact List</h1>
-            <Contact name="Hasib" email='mr.hasib45@gmail.com'/>
-            <Contact name="Swarna" email='swarna80@gmail.com'/>
-        </div>
-    );
+  function average(a:number, b:number) {
+    return a + b / 2;
+  }
+  const total = average(2,1);
+  console.log(total);
+  return (
+    <div>
+      <h1>Contact List</h1>
+      <Contact name="Hasib" email="mr.hasib45@gmail.com" />
+      <Contact name="Swarna" email="swarna80@gmail.com" />
+    </div>
+  );
 };
 
 export default Contacts;
