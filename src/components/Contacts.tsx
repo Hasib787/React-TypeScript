de3d54f648dddfab25de4782504ee43962f697f3
Md. Masud Rana <mr.hasib45@gmail.com>
2022-01-15 13:05:24 +0600

    added a form and style the input field

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Contact from "./Contact";
 
 const Contacts = () => {
+  const [contact, setContact] = useState("");
+  console.log(contact);
   
   return (
     <div>
-      <h1>Contact List</h1>
+      <h1>🦸Contact List</h1>
       <div className="form">
-          <input 
+          <input
+            className="formInput" 
             name="name"
             placeholder="Contact Name"
             type="text"
+            onChange={(e)=>setContact(e.target.value)}
           />
+          <button>Add</button>
       </div>
       <Contact name="Hasib" email="mr.hasib45@gmail.com" />
       <Contact name="Swarna" email="swarna80@gmail.com" />
