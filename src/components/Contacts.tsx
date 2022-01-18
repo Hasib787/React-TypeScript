fdc0446a78b2debffed1a30926a171ec402fa404
Md. Masud Rana <mr.hasib45@gmail.com>
2022-01-18 22:56:48 +0600

    added a function to add contact list

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -3,8 +3,13 @@ import Contact from "./Contact";
 
 const Contacts = () => {
   const [contact, setContact] = useState("");
-  console.log(contact);
-  
+  const [contactList, setContactList] = useState([]);
+
+  const handleAddContact = (e) => {
+    e.preventDefault();
+    setContactList([...contactList, contact]);
+    setContact("");
+  }
   return (
     <div>
       <h1>🦸Contact List</h1>
@@ -16,11 +21,10 @@ const Contacts = () => {
             type="text"
             onChange={(e)=>setContact(e.target.value)}
           />
-          <button>Add</button>
+          <button onClick={handleAddContact}>Add</button>
       </div>
       <Contact name="Hasib" phone="01837553116" email="mr.hasib45@gmail.com" />
       <Contact name="Swarna" phone="01837553116" email="swarna80@gmail.com" />
-      <Contact name="Rana" phone="01837553116" email="rana@gmail.com" />
       <Contact name="Zara" phone="01837553116" />
     </div>
   );
